13e75328eb6c792172770324711db9a9075c3bec
Danny Ruda <danny.ruda1997@gmail.com>
2023-01-29 23:11:34 +0100

    lifts state up from languages to app with state changing functions

--- src/components/data-fill-section/data-box-types/Languages.js
+++ src/components/data-fill-section/data-box-types/Languages.js
@@ -1,130 +1,100 @@
-import React from "react"
+import React from "react";
 
 function Languages() {
+  return (
+    <div class="data-box">
+      <h2 id="box-header">Languages</h2>
+      <form>
+        <div class="input-row">
+          <input type="text" placeholder="Language" />
+          <Select />
+          <button className="delete">X</button>
+        </div>
+        <div class="input-row">
+          <input type="text" placeholder="Language" />
+          <Select />
+          <button className="delete">X</button>
+        </div>
+        <div className="wrap-add-buttons">
+          <button class="add">+ Add Language</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function Select(props) {
+  return (
+    <select onChange={props.handleChange} value={props.value}>
+      <option>Beginner</option>
+      <option>Intermediate</option>
+      <option>Fluent</option>
+      <option>Business</option>
+      <option>Native</option>
+    </select>
+  );
+}
+
+function AddButton(props) {
+  return (
+    <div className="wrap-add-buttons">
+      <button class="add" onClick={props.handleAdd.bind(this, "languages")}>
+        + Add Language
+      </button>
+    </div>
+  );
+}
+
+class LanguagesClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.blockSubmit = this.blockSubmit.bind(this);
+  }
+
+  blockSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    const { values, handleAdd, handleChange, handleRemove } = this.props;
+    const length = values.length;
+    const inputs = values.map((value, index) => {
+      return (
+        <div class="input-row" key={index}>
+          <input
+            type="text"
+            placeholder="Language"
+            value={value.language}
+            onChange={(e) => {
+              handleChange(e, index, "language", "languages");
+            }}
+          />
+          <Select
+            handleChange={(e) => {
+              handleChange(e, index, "level", "languages");
+            }}
+            value={value.level}
+          />
+          <button
+            className="delete"
+            onClick={handleRemove.bind(this, "languages", index)}
+          >
+            X
+          </button>
+        </div>
+      );
+    });
     return (
       <div class="data-box">
         <h2 id="box-header">Languages</h2>
-        <form>
-          <div class="input-row">
-            <input type="text" placeholder="Language" />
-            <Select />
-            <button className="delete">X</button>
-          </div>
-          <div class="input-row">
-            <input type="text" placeholder="Language" />
-            <Select />
-            <button className="delete">X</button>
-          </div>
-          <div className="wrap-add-buttons">
-            <button class="add">+ Add Language</button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-
-  class LanguagesClass extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        values: [
-          { language: "", level: "" },
-          { language: "", level: "" },
-        ],
-      };
-      this.handleAdd = this.handleAdd.bind(this);
-      this.handleRemove = this.handleRemove.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-      this.blockSubmit = this.blockSubmit.bind(this);
-    }
-  
-    blockSubmit(e) {
-      e.preventDefault();
-    }
-  
-    handleAdd() {
-      console.log(this.state);
-      this.setState((state, props) => {
-        return {
-          values: state.values.concat([{ language: "", level: "" }]),
-        };
-      });
-    }
-  
-    handleRemove(index) {
-      let valuesCopy = this.state.values.slice();
-      valuesCopy.splice(index, 1);
-      this.setState({ values: valuesCopy });
-    }
-  
-    handleChange(e, index, type) {
-      const target = e.target;
-      let valuesCopy = this.state.values.slice();
-      valuesCopy[index][type] = target.value;
-      this.setState({ values: valuesCopy });
-    }
-  
-    render() {
-      const inputs = this.state.values.map((value, index) => {
-        return (
-          <div class="input-row" key={index}>
-            <input
-              type="text"
-              placeholder="Language"
-              value={value.language}
-              onChange={(e) => {
-                this.handleChange(e, index, "language");
-              }}
-            />
-            <Select
-              handleChange={(e) => {
-                this.handleChange(e, index, "level");
-              }}
-              value={value.level}
-            />
-            <button
-              className="delete"
-              onClick={this.handleRemove.bind(this, index)}
-            >
-              X
-            </button>
-          </div>
-        );
-      });
-      return (
-        <div class="data-box">
-          <h2 id="box-header">Languages</h2>
-          <div className="form no-margin">
-            <form onSubmit={this.blockSubmit}>
-              {inputs}
-              <AddButton handleAdd={this.handleAdd} />
-            </form>
-          </div>
+        <div className="form no-margin">
+          <form onSubmit={this.blockSubmit}>
+            {inputs}
+            <AddButton handleAdd={handleAdd} />
+          </form>
         </div>
-      );
-    }
-  }
-  
-  function Select(props) {
-    return (
-      <select onChange={props.handleChange} value={props.value}>
-        <option>Beginner</option>
-        <option>Intermediate</option>
-        <option>Fluent</option>
-        <option>Business</option>
-        <option>Native</option>
-      </select>
-    );
-  }
-  
-  function AddButton(props) {
-    return (
-      <div className="wrap-add-buttons">
-        <button class="add" onClick={props.handleAdd}>
-          + Add Language
-        </button>
       </div>
     );
   }
-  export { LanguagesClass };
-  
+}
+export { LanguagesClass };
