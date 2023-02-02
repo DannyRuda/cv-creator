d262b10d7af34c0bfe0d6635ee401a8d08b1e700
Danny Ruda <danny.ruda1997@gmail.com>
2023-02-02 03:52:39 +0100

    increase of indicator width is now precisely calculated from variables

--- src/components/data-fill-section/progressBar/progressBar.js
+++ src/components/data-fill-section/progressBar/progressBar.js
@@ -1,15 +1,15 @@
 import "./progressBar.css";
 import { ProgressPoint } from "./progressPoint";
-import React from "react"
+import React from "react";
 
 class ProgressBarClass extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {functionActive:false}
+    super(props);
+    this.state = { functionActive: false };
   }
 
   componentDidUpdate() {
-    changeProgressIndicator(this.props.values)
+    changeProgressIndicator(this.props.values);
   }
 
   render() {
@@ -32,7 +32,6 @@ class ProgressBarClass extends React.Component {
 }
 
 function ProgressBar(props) {
-
   return (
     <div className="progressBar-frame">
       <div className="progressBar">
@@ -51,51 +50,109 @@ function ProgressBar(props) {
 }
 
 function changeProgressIndicator(values) {
-  const {personal,contact,education,work,skills,languages} = values;
+  const { personal, contact, education, work, skills, languages } = values;
   const progressBar = document.querySelector(".progressBar");
-  let multiplicator = 1;
-  if (!boxValuesFull(personal)) {
-    for(let field in personal[0]) {
-      if(personal[0][field]) {multiplicator++}
-    }
-  } else if (boxValuesFull(personal) && !boxValuesFull(contact)) {
-    multiplicator = 6;
-    for(let field in contact[0]) {
-      if(contact[0][field]) {multiplicator++}
-    }
-  } else if(boxValuesFull(contact) && !boxValuesFull(education)) {
-    multiplicator = 11;
-    for(let field in education[0]) {
-      if(education[0][field]) {multiplicator++}
+  let portion = 0;
+  for (let field in personal[0]) {
+    if (personal[0][field]) {
+      portion += portionIncrease(personal);
     }
   }
-  else if(boxValuesFull(education) && !boxValuesFull(work)) {
-    multiplicator = 16;
-    for(let field in work[0]) {
-      if(work[0][field]) {multiplicator++}
-    }
-  } else if(boxValuesFull(work)) {
-    multiplicator = 21;
-    for(let field in skills[0]) {
-      if(skills[0][field]) {multiplicator += 2}
-    }
-    for(let field in languages[0]) {
-      if(languages[0][field]) {multiplicator += 2}
+  if(boxValuesFull(personal)) {portion = getPortionOfReachedStagesAndPoints(1)}
+
+  for (let field in contact[0]) {
+    if (contact[0][field]) {
+      portion += portionIncrease(contact);
     }
   }
-  progressBar.style.setProperty("--indicator-multiplicator",multiplicator);
+  if(boxValuesFull(contact)) {portion = getPortionOfReachedStagesAndPoints(2)}
+
+  for (let object of education) {
+    for (let field in object) {
+      if (object[field]) {
+        portion += portionIncrease(education);
+      }
+    }
+  }
+  if(boxValuesFull(education)) {portion = getPortionOfReachedStagesAndPoints(3)}
+
+  for (let object of work) {
+    for (let field in object) {
+      if (object[field]) {
+        portion += portionIncrease(work);
+      }
+    }
+  }
+  if(boxValuesFull(work)) {portion = getPortionOfReachedStagesAndPoints(4)}
+
+  for (let object of skills) {
+    for (let field in object) {
+      if (object[field]) {
+        portion += portionIncrease(skills)/2;
+      }
+    }
+  }
+
+  for (let object of languages) {
+    for (let field in object) {
+      if (object[field]) {
+        portion += portionIncrease(languages)/2;
+      }
+    }
+  }
+  if(boxValuesFull(skills) && boxValuesFull(languages)) {portion = getPortionOfReachedStagesAndPoints(5)}
+
+  progressBar.style.setProperty("--indicator-portion", portion);
 }
 
 function boxValuesFull(boxCategory) {
-  console.log("box cat",boxCategory)
+  console.log("box cat", boxCategory);
   let emptyField = false;
-  for(let object of boxCategory)
-    for(let [key,value] of Object.entries(object)) {
-      if(!value) {
+  for (let object of boxCategory)
+    for (let [key, value] of Object.entries(object)) {
+      if (!value) {
         emptyField = true;
       }
     }
-  return !emptyField
+  return !emptyField;
+}
+
+function totalStageSteps(category) {
+  let steps = 0;
+  
+    for (let object of category) {
+      steps += Object.keys(object).length;
+    }
+  
+  return steps;
+}
+
+function getPointDiameter() {
+  const progressBarStyles = getComputedStyle(
+    document.querySelector(".progressBar")
+  );
+  const stageDistance = Number(progressBarStyles.getPropertyValue("--point-diameter").split("p")[0])
+    return stageDistance;
+}
+
+function portionIncrease(category) {
+  return (1/5 * (1-4*getPortionOfOnePoint()))/totalStageSteps(category);
+}
+
+function getPortionOfOnePoint() {
+  const progressBarStyles = getComputedStyle(
+    document.querySelector(".progressBar")
+  );
+  const pointDiameter = getPointDiameter();
+  const barLength = Number(progressBarStyles.getPropertyValue("--bar-length").split("p")[0]);
+  const startPointDiameter = Number(progressBarStyles.getPropertyValue("--start-point-diameter").split("p")[0])
+  return pointDiameter/(barLength - pointDiameter - startPointDiameter);
+}
+
+function getPortionOfReachedStagesAndPoints(reachedStages) {
+  const pointPortion = getPortionOfOnePoint();
+  const stagePortion = 1/5 * (1-4*getPortionOfOnePoint());
+  return reachedStages * (pointPortion + stagePortion);
 }
 
 export { ProgressBar, ProgressBarClass };
