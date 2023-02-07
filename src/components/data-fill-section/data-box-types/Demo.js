a4e77e8be094706e0859b9d4757d2a89483dfb38
Danny Ruda <danny.ruda1997@gmail.com>
2023-02-07 23:10:59 +0100

    updates Demo box

--- src/components/data-fill-section/data-box-types/Demo.js
+++ src/components/data-fill-section/data-box-types/Demo.js
@@ -1,26 +1,25 @@
 function DemoButtons(props) {
-    const {handleAutofill,handleClear} = props;
-    return (
-      <div className="data-box">
-        <h2 id="box-header">Demo Functionality</h2>
-        <div className="box-anchor" id="Demo" ></div>
-        <Buttons handleAutofill={handleAutofill} handleClear={handleClear} />
-      </div>
-    );
-  }
-  
-  function Buttons(props) {
-    return (
-      <div className="buttons demo">
-        <button id="autofill" onClick={props.handleAutofill}>
-          Autofill
-        </button>
-        <button id="clear" onClick={props.handleClear}>
-          Clear
-        </button>
-      </div>
-    );
-  }
+  const { handleAutofill, handleClear } = props;
+  return (
+    <div className="data-box">
+      <h2 id="box-header">Demo Functionality</h2>
+      <div className="box-anchor" id="Demo"></div>
+      <Buttons handleAutofill={handleAutofill} handleClear={handleClear} />
+    </div>
+  );
+}
 
-  export { DemoButtons };
-  
+function Buttons(props) {
+  return (
+    <div className="buttons demo">
+      <button id="autofill" onClick={props.handleAutofill}>
+        Autofill
+      </button>
+      <button id="clear" onClick={props.handleClear}>
+        Clear
+      </button>
+    </div>
+  );
+}
+
+export { DemoButtons };
