147c6448aa7ceedb13e4c67cba0a3fe2385faa0e
Danny Ruda <danny.ruda1997@gmail.com>
2023-02-27 21:25:42 +0100

    improved responsiveness for small height but large width

--- src/components/cv-preview-section/cvPreview.js
+++ src/components/cv-preview-section/cvPreview.js
@@ -1,28 +1,10 @@
 import { createRef } from "react";
-import { useScreenshot } from "use-react-screenshot";
-import { saveAs } from "file-saver";
-import html2canvas from "html2canvas";
-import { jsPDF } from "jspdf";
 import { CV } from "./cv";
-import { editIcon } from "../data-fill-section/svg-components";
 import { downloadCV } from "../data-fill-section/download";
 import "./cv-preview.css";
 
 function CVPreview(props) {
   const screenshotRef = createRef();
-  const [, takeScreenshot] = useScreenshot({ type: "image/png", quality: 1.0 });
-  function screenshotWithReact() {
-    const cv = document.querySelector("#cv");
-    hideAndScaleCV(cv);
-    takeScreenshot(screenshotRef.current).then((img) => {
-      window.setTimeout(() => {
-        const pdf = new jsPDF();
-        pdf.addImage(img, "JPG", 0, 0);
-        pdf.save("download.pdf");
-        showCV(cv);
-      }, 1000);
-    });
-  }
   return (
     <div className="cv-preview">
       <CV screenshot={screenshotRef} values={props.dataBoxesValues} />
@@ -55,3 +37,20 @@ function Buttons(props) {
 }
 
 export { CVPreview };
+
+
+  /*
+    const [, takeScreenshot] = useScreenshot({ type: "image/png", quality: 1.0 });
+  function screenshotWithReact() {
+    const cv = document.querySelector("#cv");
+    hideAndScaleCV(cv);
+    takeScreenshot(screenshotRef.current).then((img) => {
+      window.setTimeout(() => {
+        const pdf = new jsPDF();
+        pdf.addImage(img, "JPG", 0, 0);
+        pdf.save("download.pdf");
+        showCV(cv);
+      }, 1000);
+    });
+  }
+  */
